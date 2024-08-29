564cdedced226a39352ce8a400bd09da32291e49
ROSHAN KUMAR YADAV <roshan099999@gmail.com>
2024-08-29 16:48:06 +0530

    changes in herosection

--- frontend/src/components/Herosection2.js
+++ frontend/src/components/Herosection2.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import SearchButton from './SearchButton';
+import '../css/HeroSection2.css'
+
 
 const Herosection2 = () => {
   return (
-    <div className='main-section'>
-        Herosection2
+    <div className="main-section">
+      {/* Part 1 contain button and heading  */}
+      <div className="part1">
+        <div>
+          <h1>Find Popular Trips</h1>
+        </div>
+        <div className='buttonAndtext'>
+          <p>
+            Whether you are looking for places for a vacation. We are here to
+          </p>
+          <SearchButton text="View More"/>
+          
+        </div>
+
+        
+      </div>
     </div>
-  )
+  );
 }
+
 
 export default Herosection2
